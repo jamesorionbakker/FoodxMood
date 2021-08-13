34926b8837a0c9c41dcd12498239fcb47bd4a7d8
James Bakker <james.orion.bakker@gmail.com>
2021-08-13 16:42:57 -0700

    updated import for main stylesheet

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './styles.scss';
+import './styles/main.scss';
 import { Provider } from 'react-redux';
 import store from './components/common/state/Store.js'
 
