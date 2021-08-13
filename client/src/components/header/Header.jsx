75c5f776cdb8308d965726b7d3fe48aa68f7c4d7
James Bakker <james.orion.bakker@gmail.com>
2021-08-13 16:41:10 -0700

    wrapped logo in h4 tag

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -16,7 +16,7 @@ export default function Header() {
         <Container fluid className="header-container">
             <Row className="header">
                 <Col xs={12} sm="auto" className="logo">
-                    Food <span className="x" >x</span> Mood
+                    <h4>Food <span className="x" >x</span> Mood</h4>
                 </Col>
                 <Col xs={12} md={true} className="filler"></Col>
                 <Col xs={12} md={'auto'} className="login">
