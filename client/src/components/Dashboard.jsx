845dbfe74c48aa18f61e9040af8003d7536501bd
James Bakker <james.orion.bakker@gmail.com>
2021-08-13 10:24:34 -0700

    removed unused variables

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Sidebar from './Sidebar';
 import Activity from './activity/Activity'
 import { useSelector } from 'react-redux';
--- client/src/components/IngredientPill.jsx
+++ client/src/components/IngredientPill.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useState } from 'react';
 import './IngredientPill.scss';
 
 export default function IngredientPill(props) {
