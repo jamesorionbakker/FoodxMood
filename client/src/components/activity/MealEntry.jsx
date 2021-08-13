dcafea68ae28effefe472b841d930dff43770c54
James Bakker <james.orion.bakker@gmail.com>
2021-08-13 16:42:18 -0700

    refactored emoji generating function, moved edit links to new col after main col

--- client/src/components/activity/MealEntry.jsx
+++ client/src/components/activity/MealEntry.jsx
@@ -11,43 +11,52 @@ export default function MealEntry(props) {
     let { entry } = props;
     let { ingredients, mealType, time } = entry;
 
-    function getEmoji(mealType){
-        const mealEnum = {
-            'Breakfast': '🍳',
-            'Lunch': '🥪',
-            'Dessert': '🧁',
-            'Snack': '🍇',
-            'Dinner': '🍴',
-            'Drink': '☕'
-        }
-        return mealEnum[mealType];
+    function getEmoji(type) {
+        return {
+            Breakfast: '🍳',
+            Lunch: '🥪',
+            Dessert: '🧁',
+            Snack: '🍇',
+            Dinner: '🍴',
+            Drink: '🥤',
+            HealthCheck: '❤️',
+        }[type];
     }
 
     return (
         <Container className="entry-container" fluid>
             <Row>
-                <Col xs="auto" className="time-container">{dayjs.unix(time).format('h:mm A')}</Col> 
+                <Col xs="auto" className="time-container">
+                    {dayjs.unix(time).format('h:mm A')}
+                </Col>
                 <Col xs={true} className="entry-data-container meal-entry-container">
-                        <Row className="entry-heading">
-                            <Col>
-                                <h5>
-                                {getEmoji(mealType)} {mealType}
-                                </h5>
-                            </Col>
-                            <Col xs="auto">
-                                <ManageEntry entry={entry} />
-                            </Col>
-                        </Row>
-                        <Row className="entry-section">
-                            <Col xs="auto">
-                                <div className="section-title">Ingredients:</div>
-                            </Col>
-                            <Col xs={true}>
-                                {ingredients.map((ingredient, index) => {
-                                    return <Pill key={index} text={ingredient.name} color="red" />;
-                                })}
-                            </Col>
-                        </Row>
+                    <Row>
+                        <Col>
+                            <Row className="entry-heading">
+                                <Col>
+                                    <h5>
+                                        <span className="emoji">{getEmoji(mealType)}</span>
+                                        {mealType}
+                                    </h5>
+                                </Col>
+                            </Row>
+                            <Row className="entry-section">
+                                <Col xs="auto">
+                                    <div className="section-title">Ingredients:</div>
+                                </Col>
+                                <Col xs={true}>
+                                    {ingredients.map((ingredient, index) => {
+                                        return (
+                                            <Pill key={index} text={ingredient.name} color="red" />
+                                        );
+                                    })}
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col className="manage-entry-col" xs="auto">
+                            <ManageEntry entry={entry} />
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
         </Container>
