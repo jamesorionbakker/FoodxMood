0ea7b04b2108fa065d21956f67b243ebda344048
James Bakker <james.orion.bakker@gmail.com>
2021-08-13 09:22:31 -0700

    added password validation

--- client/src/components/common/utils/formValidation.js
+++ client/src/components/common/utils/formValidation.js
@@ -1,21 +1,30 @@
-import {validate as validateEmail } from 'email-validator'
+import { validate as validateEmail } from 'email-validator';
 
-export function password(value){
-    let valid = /^\w{1,}$/i.test(value)
-    let error = valid ? '' : 'Invalid Password'
-    return {valid, error}
+export function password(value) {
+    let errorMessage = 'Password must be at least 8 characters with at least one capital letter and one number'
+    if (/\s/.test(value)) {
+        return { valid: false, error: 'Password cannot contain spaces. ' + errorMessage }
+    }
+    if (!/^\S{8,}$/i.test(value)) {
+        return { valid: false, error: errorMessage };
+    }
+    if (!/[A-Z]/.test(value)) {
+        return { valid: false, error: errorMessage }
+    }
+    if (!/\d/.test(value)) {
+        return { valid: false, error: errorMessage }
+    }
+    return { valid: true, error: '' };
 }
 
-export function email(value){
-
-    let valid = validateEmail(value)
-    let error = valid ? '' : 'Invalid Email Address'
-    return {valid, error}
-
+export function email(value) {
+    let valid = validateEmail(value);
+    let error = valid ? '' : 'Invalid Email Address';
+    return { valid, error };
 }
 
-export function name(value){
-    let valid = /^\w{1,}/i.test(value)
-    let error = valid ? '' : 'Invalid Name'
-    return {valid, error}
+export function name(value) {
+    let valid = /^\w{1,}/i.test(value);
+    let error = valid ? '' : 'Invalid Name';
+    return { valid, error };
 }
