cb7cb33ac9ab0b867bbd42026fec1611f4f6620d
James Bakker <james.orion.bakker@gmail.com>
2021-08-15 13:14:01 -0700

    added 'attempting login' state to hide login form until after token login is attempted

--- client/src/components/common/state/UserStateReducer.js
+++ client/src/components/common/state/UserStateReducer.js
@@ -1,11 +1,11 @@
 import UserState from 'components/common/utils/userState'
 
-const initialState = new UserState()
+const initialState = {...new UserState(), attemptingLogin: true}
 
 export default function UserStateReducer(state = initialState, action){
     switch (action.type){
         case 'USERSTATE/SET':
-            return action.payload
-        default: return state
+            return {...action.payload, attemptingLogin: false}
+        default: return {...state }
     }
 }
--- client/src/components/login_form/LoginForm.jsx
+++ client/src/components/login_form/LoginForm.jsx
@@ -11,67 +11,67 @@ import RegisterForm from 'components/register_form/registerForm';
 
 export default function Login() {
     let state = useSelector((state) => state.loginForm);
+    let { attemptingLogin } = useSelector((state) => state.UserState);
 
     const dispatch = useDispatch();
 
     return (
         <div className="login-container">
-            <Form
-                className="form"
-                onSubmit={(e) => {
-                    e.preventDefault()
-                    console.log('signing in');
-                    dispatch(logIn());
-                }}>
-                <div className="login">
-                    <Row className="form-row">
-                        <Col className="form-col" md="auto">
-                            <Form.Control
-                                isInvalid={state.invalidUsername}
-                                name="username"
-                                className="username"
-                                placeholder="User Name"
-                                value={state.username}
-                                onChange={(e) => {
-                                    dispatch(setLoginForm({ username: e.target.value }));
-                                }}
-                            />
-                        </Col>
-                        <Col className="filler" xs={12}></Col>
-                        <Col className="form-col" md="auto">
-                            <Form.Control
-                                isInvalid={state.invalidPassword}
-                                name="password"
-                                className="password"
-                                type="password"
-                                value={state.password}
-                                onChange={(e) => {
-                                    dispatch(setLoginForm({ password: e.target.value }));
-                                }}
-                                placeholder="Password"
-                            />
-                        </Col>
-                        <Col className="filler" xs={12}></Col>
-                        <Col className="form-col" xs={8} md="auto">
-                            <Button
-                                type="submit"
-                                className="login-button"
-                                variant="light">
-                                Sign In
-                            </Button>
-                        </Col>
-                        <Col className="form-col" xs={4} md="auto">
-                            <Button
-                                onClick={() => dispatch(showRegisterForm())}
-                                className="button-dark-green"
-                                variant="success">
-                                Sign Up
-                            </Button>
-                            <RegisterForm />
-                        </Col>
-                    </Row>
-                </div>
-            </Form>
+            {!attemptingLogin && (
+                <Form
+                    className="form"
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        console.log('signing in');
+                        dispatch(logIn());
+                    }}>
+                    <div className="login">
+                        <Row className="form-row">
+                            <Col className="form-col" md="auto">
+                                <Form.Control
+                                    isInvalid={state.invalidUsername}
+                                    name="username"
+                                    className="username"
+                                    placeholder="User Name"
+                                    value={state.username}
+                                    onChange={(e) => {
+                                        dispatch(setLoginForm({ username: e.target.value }));
+                                    }}
+                                />
+                            </Col>
+                            <Col className="filler" xs={12}></Col>
+                            <Col className="form-col" md="auto">
+                                <Form.Control
+                                    isInvalid={state.invalidPassword}
+                                    name="password"
+                                    className="password"
+                                    type="password"
+                                    value={state.password}
+                                    onChange={(e) => {
+                                        dispatch(setLoginForm({ password: e.target.value }));
+                                    }}
+                                    placeholder="Password"
+                                />
+                            </Col>
+                            <Col className="filler" xs={12}></Col>
+                            <Col className="form-col" xs={8} md="auto">
+                                <Button type="submit" className="login-button" variant="light">
+                                    Sign In
+                                </Button>
+                            </Col>
+                            <Col className="form-col" xs={4} md="auto">
+                                <Button
+                                    onClick={() => dispatch(showRegisterForm())}
+                                    className="button-dark-green"
+                                    variant="success">
+                                    Sign Up
+                                </Button>
+                                <RegisterForm />
+                            </Col>
+                        </Row>
+                    </div>
+                </Form>
+            )}
         </div>
     );
 }
