27ffefa50a2c4d3e476be06714039bf89006cfa6
James Bakker <james.orion.bakker@gmail.com>
2021-08-16 17:35:00 -0700

    moved buttons outside of loading pane of activity component

--- client/src/components/activity/Activity.jsx
+++ client/src/components/activity/Activity.jsx
@@ -7,28 +7,32 @@ import * as FeedArray from './utils/feedArray';
 import { useDispatch, useSelector } from 'react-redux';
 import { setActivity } from 'components/activity/state/ActivityActions';
 import HealthCheckForm from 'components/health_check_form/HealthCheckForm';
-import  Spinner from 'react-bootstrap/Spinner';
+import Spinner from 'react-bootstrap/Spinner';
 
 export default function Activity(props) {
     const dispatch = useDispatch();
     let state = useSelector((state) => state);
-    let loaded = !state.activity.loading
-    
-    useEffect( () => {
+    let loaded = !state.activity.loading;
+
+    useEffect(() => {
         dispatch(setActivity());
     }, [state.view.currentView, state.activity.filter]);
-    
+
     return (
         <div className="activity-container">
-            {loaded ? <div>
-                <AddButtonsContainer />
-                <MealForm />
-                <HealthCheckForm />
-                <hr />
-                {FeedArray.build(state.activity.data).map((dateBlock, index) => {
-                    return <DateBlock key={index} index={index} entries={dateBlock} />;
-                })}
-            </div> : <Spinner animation='border' className="loading-spinner"/>}
+            <AddButtonsContainer />
+            <MealForm />
+            <HealthCheckForm />
+            {loaded ? (
+                <div>
+                    <hr />
+                    {FeedArray.build(state.activity.data).map((dateBlock, index) => {
+                        return <DateBlock key={index} index={index} entries={dateBlock} />;
+                    })}
+                </div>
+            ) : (
+                <Spinner animation="border" className="loading-spinner" />
+            )}
         </div>
     );
 }
